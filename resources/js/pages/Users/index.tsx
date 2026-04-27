39af8bf4d875d96eea949c0fac0c28de5466f632
Muhammad Taufiq <prasetyotaufiq697@gmail.com>
2026-04-27 13:04:14 +0700

    First

---
+++ resources/js/pages/Users/index.tsx
@@ -0,0 +1,23 @@
+import { Head } from '@inertiajs/react';
+import { index as userIndexRoute } from '@/routes/users';
+
+
+export default function UsersIndex() {
+    return (
+        <>
+            <Head title="Manage User" />
+            <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+
+            </div>
+        </>
+    );
+}
+
+UsersIndex.layout = {
+    breadcrumbs: [
+        {
+            title: 'Manage User',
+            href: userIndexRoute(),
+        },
+    ],
+};
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Features;
+use App\Http\Controllers\UserController;
+
+Route::inertia('/', 'welcome', [
+    'canRegister' => Features::enabled(Features::registration()),
+])->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+});
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+});
+
+require __DIR__.'/settings.php';
